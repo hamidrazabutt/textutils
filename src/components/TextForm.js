bea20d4ae79fb342f3d939ae1612e4a6f7036af7
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2021-11-15 07:15:28 +0500

    Dart & Light Mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -38,13 +38,22 @@ export default function TextForm(props) {
   //  setText("new text"); // Correct way to change tghe state
   return (
     <>
-      <div>
+      <div
+        className="container"
+        style={{
+          color: props.mode === 'dark' ? 'white' : '#042743',
+        }}
+      >
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
             className="form-control"
             value={text}
             onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === 'dark' ? 'grey' : 'white',
+              color: props.mode === 'dark' ? 'white' : '#042743',
+            }}
             id="myBox"
             rows="8"
           ></textarea>
@@ -65,14 +74,23 @@ export default function TextForm(props) {
           Remove Extra Spaces
         </button>
       </div>
-      <div className="container my-3">
+      <div
+        className="container my-3"
+        style={{
+          color: props.mode === 'dark' ? 'white' : '#042743',
+        }}
+      >
         <h2>Your text summary</h2>
         <p>
           {text.split(' ').length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(' ').length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>
+          {text.length > 0
+            ? text
+            : 'Enter something in the textbox above to preview it here'}
+        </p>
       </div>
     </>
   );
