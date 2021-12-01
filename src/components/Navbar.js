a23375957ac48e9c2b8a4d2aca97acd026cc0496
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2021-12-02 04:46:59 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="\">
+        <Link className="navbar-brand" to="/">
           {props.title}
         </Link>
         <button
@@ -25,12 +25,12 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="home">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" href="/about">
+              <Link className="nav-link" to="/about">
                 {props.aboutText}
               </Link>
             </li>
